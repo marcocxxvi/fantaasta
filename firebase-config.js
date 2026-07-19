b42bac710e55c61b6bdfcf6eb6b9d5de2881f870
marcocxxvi <markhero04@gmail.com>
2026-07-19 17:27:08 +0200

    Update and rename firebase-config[1].js to firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyA9CjgWg5jmRG4EoLMGJ3Fn9Apezu784I0",
+  authDomain: "fanta-asta-4f67b.firebaseapp.com",
+  projectId: "fanta-asta-4f67b",
+  storageBucket: "fanta-asta-4f67b.firebasestorage.app",
+  messagingSenderId: "245290291255",
+  appId: "1:245290291255:web:519a7d6505fbe7b1212fb2"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
